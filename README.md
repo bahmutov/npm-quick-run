2330551b7c9b12bfd5570bc1e9b9bffcb51d7ffe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-29 23:10:07 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # npm-quick-run
+
+> Quickly run NPM script by prefix without typing the full name
+
+
---
+++ bin/npm-quick-run.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node --harmony
+
+'use strict';
+
+const help = [
+  'use: nrun <npm script prefix> <script arguments>',
+  'for example, run tests with "nrun t"',
+  'or if you have "lint" script run it with "nrun l"'
+].join('\n');
+
+require('simple-bin-help')({
+  minArguments: 3,
+  packagePath: __dirname + '/../package.json',
+  help: help
+});
+
+const prefix = process.argv[2];
+console.log('running prefix', prefix);
