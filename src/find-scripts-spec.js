23504f4409a316eb807afd757a9f6aa1c991bd6a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-29 23:27:16 -0500

    chore(unit): unit testing find scripts

--- src/find-scripts-spec.js
+++ src/find-scripts-spec.js
@@ -1,6 +1,38 @@
 describe('find-scripts', function () {
   const find = require('./find-scripts');
+  const scripts = {
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz'
+  };
+
   it('is a function', function () {
     console.assert(typeof find === 'function');
+  });
+
+  it('finds nothing', function () {
+    const found = find('a', scripts);
+    console.assert(Array.isArray(found));
+    console.assert(found.length === 0);
+  });
+
+  it('finds 1 partial match', function () {
+    const found = find('f', scripts);
+    console.assert(Array.isArray(found));
+    console.assert(found.length === 1);
+    console.assert(found[0] === 'foo');
+  });
+
+  it('finds 1 full match', function () {
+    const found = find('foo', scripts);
+    console.assert(Array.isArray(found));
+    console.assert(found.length === 1);
+    console.assert(found[0] === 'foo');
+  });
+
+  it('finds 2', function () {
+    const found = find('b', scripts);
+    console.assert(Array.isArray(found));
+    console.assert(found.length === 2);
   });
 });
