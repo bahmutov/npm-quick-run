c993d97db56d8d1933808f8836d130a4d58300b1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-01 11:43:03 -0500

    fix(exact): Returning first and only if exact match
    closes #4

--- src/find-scripts-spec.js
+++ src/find-scripts-spec.js
@@ -3,6 +3,7 @@ describe('find-scripts', function () {
   const scripts = {
     foo: 'foo',
     bar: 'bar',
+    barMore: 'bar-more',
     baz: 'baz'
   };
 
@@ -30,9 +31,16 @@ describe('find-scripts', function () {
     console.assert(found[0] === 'foo');
   });
 
-  it('finds 2', function () {
+  it('finds several', function () {
     const found = find('b', scripts);
     console.assert(Array.isArray(found));
-    console.assert(found.length === 2);
+    console.assert(found.length === 3);
+  });
+
+  it('finds an exact match if possible', function () {
+    const found = find('bar', scripts);
+    console.assert(Array.isArray(found));
+    console.assert(found.length === 1, found);
+    console.assert(found[0] === 'bar');
   });
 });
--- src/find-scripts.js
+++ src/find-scripts.js
@@ -1,10 +1,27 @@
 function startsWith(prefix, str) {
+  console.assert(typeof str === 'string', 'expected string', str);
   return str.indexOf(prefix) === 0;
 }
 
+function sameLength(a, b) {
+  return a.length === b.length;
+}
+
+function matchesExactly(prefix, str) {
+  return startsWith(prefix, str) &&
+    sameLength(prefix, str);
+}
+
 function findScripts(prefix, scripts) {
+  const labels = Object.keys(scripts);
+  const matchesExactlyPrefix = matchesExactly.bind(null, prefix);
+  const exactMatches = labels.filter(matchesExactlyPrefix);
+  if (exactMatches.length === 1) {
+    return exactMatches;
+  }
+
   const startsWithPrefix = startsWith.bind(null, prefix);
-  const matchingScripts = Object.keys(scripts).filter(startsWithPrefix);
+  const matchingScripts = labels.filter(startsWithPrefix);
   return matchingScripts;
 }
 
