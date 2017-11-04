68938d680bf755101f9e517696aa820d7e197802
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-04 15:24:53 -0400

    chore: setup e2e tests

---
+++ __snapshots__/bin-spec.js
@@ -0,0 +1,23 @@
+exports['quick run bin shows available scripts with prefix 1'] = `
+  stdout:
+  -------
+  running command with prefix "t"
+  -------
+  stderr:
+  -------
+  Several scripts start with "t" test, test-foo
+  -------
+
+`
+
+exports['quick run bin runs by prefix 1'] = `
+  stdout:
+  -------
+  running command with prefix "ech"
+
+  > echo hello
+
+  hello
+  -------
+
+`
---
+++ src/bin-spec.js
@@ -0,0 +1,24 @@
+const execaWrap = require('execa-wrap')
+const snapshot = require('snap-shot-it')
+const {join} = require('path')
+
+/* eslint-env mocha */
+describe('quick run bin', () => {
+  const bin = join(__dirname, '..', 'bin', 'npm-quick-run.js')
+
+  it('shows available scripts with prefix', () => {
+    return execaWrap('node', [bin, 't'], {filter: ['stdout', 'stderr']})
+      .then(snapshot)
+  })
+
+  it('runs by prefix', () => {
+    const removePath = (s) => {
+      return s.split('\n')
+        .filter(s => !s.includes('> npm-quick-run'))
+        .join('\n')
+    }
+    return execaWrap('node', [bin, 'ech'], {filter: ['stdout', 'stderr']})
+      .then(removePath)
+      .then(snapshot)
+  })
+})
