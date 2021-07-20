e276f104fe0e3305d81c05356061b926832340ed
Daniel P. Shannon <daniel@fleet.i.ng>
2021-07-20 09:14:58 -0400

    test: Yarn support

--- src/quick-run-spec.js
+++ src/quick-run-spec.js
@@ -1,7 +1,39 @@
+const mockFS = require('mock-fs')
+const execaWrap = require('execa-wrap')
+const snapshot = require('snap-shot-it')
+
 /* global describe, it */
 describe('quick run', () => {
   const quickRun = require('./quick-run')
   it('is a function', () => {
     console.assert(typeof quickRun === 'function')
+  })
+
+  it('runs npm when package-lock.json is present', () => {
+    return new Promise(resolve => {
+      mockFS({
+        'package-lock.json': {}
+      })
+      resolve()
+    }).then(() => {
+      let pm = quickRun.getPackageManager()
+      console.assert(pm === 'npm')
+    }).finally(() => {
+      mockFS.restore()
+    })
+  })
+
+  it('runs yarn when yarn.lock is present', () => {
+    return new Promise(resolve => {
+      mockFS({
+        'yarn.lock': {}
+      })
+      resolve()
+    }).then(() => {
+      let pm = quickRun.getPackageManager()
+      console.assert(pm === 'yarn')
+    }).finally(() => {
+      mockFS.restore()
+    })
   })
 })
